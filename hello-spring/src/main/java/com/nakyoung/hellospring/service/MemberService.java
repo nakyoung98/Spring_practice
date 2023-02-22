e298bbc41f84b9f1b9392a7b37b167c60ca1b5c9
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-02-23 02:14:26 +0900

    회원서비스 개발 및 테스트케이스 작성

---
+++ hello-spring/src/main/java/com/nakyoung/hellospring/service/MemberService.java
@@ -0,0 +1,49 @@
+package com.nakyoung.hellospring.service;
+
+import com.nakyoung.hellospring.domain.Member;
+import com.nakyoung.hellospring.repository.MemberRepository;
+import com.nakyoung.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+// service class의 함수 네이밍은 비즈니스적으로 짓는 것이 좋음
+// Test를 쉽게 만드는 법 -> 클래스명에 커서대로 ctrl+shift+t(est) -> test생성으로 넘어감
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemoryMemberRepository memoryMemberRepository){
+        this.memberRepository = memoryMemberRepository;
+    }
+
+    /**
+     * 회원가입
+     * **/
+    public Long join(Member member){
+        //중복 이름 회원 X
+
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //만약 함수로 뽑는게 좋다면, 만들어놓은 코드를 긁은다음 ctrl+alt+m하면 되고, 만약 뭘 처리할지 아직 못결정했으면 ctrl+alt+shift+T 하면 할 행동 선택 가능
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                //optional로 감쌌기 때문에 ifPresent로 null 여부 체크, null이 아닐 경우 예외처리하는 람다 처리가능
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ hello-spring/src/test/java/com/nakyoung/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,79 @@
+package com.nakyoung.hellospring.service;
+
+import com.nakyoung.hellospring.domain.Member;
+import com.nakyoung.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memoryMemberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memoryMemberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memoryMemberRepository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memoryMemberRepository.clearStore();
+    }
+    //Test 코드는 한글로도 많이 적음
+
+    @Test
+    void 회원가입() {
+        //given (주어진 것)
+        Member member = new Member();
+        member.setName("나경");
+
+        //when (이것을 실행했을 때)
+        Long saveId = memberService.join(member);
+
+        //then (이 결과가 나와야 함)
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("나경");
+
+            //shift+f6
+        Member member2 = new Member();
+        member2.setName("나경");
+
+        //when
+        memberService.join(member1);
+
+        //단순한 예외처리
+//        try {
+//            memberService.join(member2); //예외가 기대됨
+//            fail("예외가 발생해야함");
+//        }catch (IllegalStateException e){
+//            assertThat(e.getMessage()).isEqualTo("");
+//        }
+
+        //편리한 예외처리
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+            //오류
+        assertThrows(NullPointerException.class, () -> memberService.join(member2));
+        //then
+    }
+
+    @Test
+    void 멤버찾기() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
